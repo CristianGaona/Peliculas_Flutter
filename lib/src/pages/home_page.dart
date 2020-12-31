f4add94b73c3f8fe0bee1cd848f24abd3cddf0ce
CristianGaona <crgaonas24@gmail.com>
2020-12-30 22:56:28 -0500

    Agregando StreamBuilder para crear un infinity Scroll

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:peliculas/src/providers/peliculas_provider.dart';
 import 'package:peliculas/src/widgets/card_swiper_widget.dart';
+import 'package:peliculas/src/widgets/movie_horizontal.dart';
 
 
 class HomePage extends StatelessWidget {
   final peliculasProvider = new PeliculasProvider();
   @override
   Widget build(BuildContext context) {
+    peliculasProvider.getPopulares();
 
      
     return Scaffold(
@@ -20,9 +22,14 @@ class HomePage extends StatelessWidget {
       
       ),
       body : Container(
+        
         child: Column(
+        
+         mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: <Widget>[
-             _swiperTarjetas()
+             _swiperTarjetas(),
+             _footer(context)
+
           ]
          
         )
@@ -59,6 +66,35 @@ return FutureBuilder(
     peliculas: [1, 2, 3, 4, 5],
   );*/
   
+}
+
+Widget _footer(BuildContext context){
+  return Container(
+    width:  double.infinity,
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        //Text('Populares', style: Theme.of(context).textTheme.subtitle1),
+        Container(
+          padding: EdgeInsets.only(left:20.0),
+          child: Text('Populares', style: Theme.of(context).textTheme.subtitle1)),
+        SizedBox(height: 5.0,),
+
+        StreamBuilder(
+          stream: peliculasProvider.popularesStream,
+          builder: (BuildContext context, AsyncSnapshot snapshot){
+
+            if ( snapshot.hasData){
+              return MovieHorizontal(peliculas:snapshot.data, siguientePagina: peliculasProvider.getPopulares);
+            }else{
+              return Center(child: CircularProgressIndicator());
+            }
+          },
+        ),
+      ]
+    )
+  );
+
 }
 
 }
--- lib/src/providers/peliculas_provider.dart
+++ lib/src/providers/peliculas_provider.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:convert';
 
 import 'package:peliculas/src/models/pelicula_model.dart';
@@ -9,6 +10,30 @@ class PeliculasProvider{
     String _url = 'api.themoviedb.org';
     String _language = 'es-ES';
 
+    int _popularesPage = 0;
+
+    List<Pelicula> _populares = new List();
+    final _popularesStreamController = StreamController<List<Pelicula>>.broadcast();
+
+
+    Function(List<Pelicula>) get  popularesSink => _popularesStreamController .sink.add;
+    Stream<List<Pelicula>> get popularesStream => _popularesStreamController .stream;
+
+
+
+    void disposeStreams(){
+      _popularesStreamController ?.close();
+    }
+
+
+    Future<List<Pelicula>> _precesarRespuesta(Uri url) async {
+      final resp = await http.get( url );
+      final decodedData = json.decode(resp.body);
+
+      //print( decodedData['results']);
+      final peliculas = new Peliculas.fromJsonList(decodedData['results']);
+      return peliculas.items;
+    }
 
     Future<List<Pelicula>> getEnCines() async {
       final url =Uri.https(_url, '3/movie/now_playing', {
@@ -16,18 +41,29 @@ class PeliculasProvider{
         'language' : _language
 
       });
-      final resp = await http.get( url );
-      final decodedData = json.decode(resp.body);
-
-      //print( decodedData['results']);
-      final peliculas = new Peliculas.fromJsonList(decodedData['results']);
-      return peliculas.items;
-
-
-
-
+      return await _precesarRespuesta(url);
 
     }
 
-    
+    Future<List<Pelicula>> getPopulares() async{
+
+      _popularesPage++;
+      final url =Uri.https(_url, '3/movie/popular', {
+        'api_key' : _apiKey,
+        'language' : _language,
+        'page'      : _popularesPage.toString()
+
+      });
+
+      final resp =  await _precesarRespuesta(url);
+      _populares.addAll(resp);
+      popularesSink(_populares);
+      return resp;
+    }
+
+
+
+
+
+
 }
